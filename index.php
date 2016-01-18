07567798cbeb079c4ff734f9cf448a2dbdd38ef0
mmohl <mmohammadl.lpkia@gmail.com>
2016-01-18 15:48:31 +0700

    tambah eloquent

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
   require 'vendor/autoload.php';
+  
+  $app = new \Slim\Slim();
 
-  $app = new Slim/Slim();
-
-  $app->get('/', function(){
+  $app->get("/", function(){
     echo 'helloworld';
   });
 
